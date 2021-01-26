39b9ef5eddad48b3d2009a93514881d7769efcd6
Nasir <movlamovnasir2000@gmail.com>
2021-01-26 17:29:00 +0400

    new new'

--- src/components/MemberArea.js
+++ src/components/MemberArea.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
 import "../assets/css/memberArea.css"
 import avatar from "../assets/images/avatar.jpg"
 import Button1 from "./Button1"
 import { Link, Route, BrowserRouter as Router,Switch } from "react-router-dom"
+import gogerti from "../assets/images/gogerti.jpg"
 
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
+import LockIcon from '@material-ui/icons/Lock';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DescriptionIcon from '@material-ui/icons/Description';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import ItemCard from './ItemCard'
 function cabinet() {
     
     const imgHandler = {
@@ -33,23 +50,77 @@ function cabinet() {
 
 function contacts() {
     return (
-        <div className="cabinetCont">
-            <p className="title">Account</p>
+        <div className="cabinetCont contactCont">
+            <p className="title">Contact</p>
             <div className="gridCont">
-                <p className="name key">* Name and Surname</p> <input className="value"   type="text" name="" id=""/>
-                <p className="email key ">* Email</p> <input  className="value"  type="email" name="" id=""/>
-                <p className="phone key">* Phone</p> <input  className="value"  type="tel" name="" id=""/>
-                <p className="date key">* Birthday</p> <input  className="value"  type="date" name="" id=""/>
+                <p className="name key">* Name and Surname</p> <input placeholder="Name and Surname" className="value"   type="text" name="" id=""/>
+                <p className="email key ">* Email</p> <input placeholder="Email" className="value"  type="email" name="" id=""/>
+                <p className="phone key">* Phone</p> <input placeholder="Telephone" className="value"  type="tel" name="" id=""/>
+                <p className="date key">* Birthday</p> <input value="1980-08-26"  className="value"  type="date" name="" id=""/>
             </div>
+            <Button1 value="save" color="#FF7A2C"/>
+
         </div>
     )
 }
-function password(){return (null) }
-function address(){return (null) }
-function bookmarks(){return (null) }
-function orders(){return (null) }
-function reminders(){return (null) }
-function bonuses(){return (null) }
+function password(){return (
+<div className="cabinetCont contactCont">
+    <p className="title">Password</p>
+    <div className="gridCont">
+        <p className="name key">* Old Password</p> <input placeholder="Old Password" className="value"   type="text" name="" id=""/>
+        <p className="email key ">* New Password</p> <input placeholder="New Password"  className="value"  type="email" name="" id=""/>
+    </div>
+    <Button1 value="save" color="#FF7A2C"/>
+</div>
+) }
+function address(){return (
+    <div className="cabinetCont address">
+        <p className="title">My Addresses</p>
+        <p className="myAdress">The list of addresses in your account is empty.</p>
+        <Button1 value="New Address" color="#FF7A2C"/>
+    </div>
+) }
+function bookmarks(){return (
+    <div className="cabinetCont bookmarks ">
+        <p className="title">Featured Products</p>
+        <p className="selection"><button className="button"><HelpOutlineIcon/> Assortment for: <p>all dates <ArrowRightIcon/></p> </button></p>
+        <div className="gridCont1">
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+        </div>
+    </div>
+) }
+function orders(){return (
+    <div className="cabinetCont orders bookmarks ">
+        <p className="title">Order history</p>
+        <div className="gridCont1">
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+            {<ItemCard image={gogerti} title="Altai sunflower oil_500 ml." desc="from Maria Fursenko" price={100} weight="50gr" discount={0}/>}
+        </div>
+    </div>
+) }
+function reminders(){return (
+    <div className="cabinetCont orders bookmarks reminders">
+        <p className="title">My notifications</p>
+        <p className="subTitle">Add an alert by e-mail or SMS, and we will remind you to place an order.</p>
+        <div className="gridCont1">
+            <p className="alerts">You have no notfications!</p>
+        </div>
+    </div>
+) }
+function bonuses(){return (
+    <div className="cabinetCont address">
+        <p className="title">My Addresses</p>
+        <p className="myAdress">The list of addresses in your account is empty.</p>
+        <Button1 value="New Address" color="#FF7A2C"/>
+    </div>
+) }
 function deposites(){return (null) }
 function freeShiping(){return (null) }
 function reviews(){return (null) }
@@ -60,24 +131,33 @@ function output(){return (null) }
 
 
 function MemberArea() {
+
+    const clickHandler = (num) => {
+            for (let i = 1; i < 13; i++) {
+                document.getElementById(`btn${i}`).setAttribute('style' , "color: #7d7068;border-left: 3px solid transparent;")
+            }
+            document.getElementById(`btn${num}`).setAttribute('style' , "color: #ff7a2c;border-left: 3px solid #ff7a2c;")
+    }
+
     return (
         <Router>
         <div className="memberAreaCont">
             
             <aside className="aside">
-                <Link to="/memberarea/cabinet" className="cabinet">                 <img src="cabinet" alt=""/> Cabinet                 </Link>
-                <Link to="/memberarea/contacts" className="contact">               <img src="contact" alt=""/> Contact                 </Link>
-                <Link to="/memberarea/password" className="password">            <img src="password" alt=""/> Password             </Link>
-                <Link to="/memberarea/address" className="address">                <img src="address" alt=""/> Address                </Link>
-                <Link to="/memberarea/bookmarks" className="bookmarks">         <img src="bookmarks" alt=""/> Bookmarks         </Link>
-                <Link to="/memberarea/orders" className="orders">                    <img src="orders" alt=""/> Orders                   </Link>
-                <Link to="/memberarea/reminders" className="reminders"  >         <img src="reminders" alt=""/> Reminders                            </Link>
-                <Link to="/memberarea/bonuses" className="bonuses">               <img src="bonuses" alt=""/> Bon   uses                   </Link>
-                <Link to="/memberarea/deposite" className="deposites">            <img src="deposites" alt=""/> Deposites               </Link>
-                <Link to="/memberarea/free-shipping" className="freeShipping">          <img src="freeShipping" alt=""/>Free shipping                    </Link>
-                <Link to="/memberarea/reviews" className="reviews">               <img src="reviews" alt=""/> Reviews                    </Link>
-                <Link to="/memberarea/recommendations" className="recommendatios">     <img src="recommendations" alt=""/> Recommendations                   </Link>
-                <Link to="/memberarea/output" className="output">                 <img src="outputs" alt=""/> Outputs                  </Link>
+                <Link to="/memberarea/cabinet" className="cabinet"><button id="btn1" onClick={() => clickHandler(1)}>                 <AccountCircleIcon/>  Cabinet                </button> </Link>
+                <Link to="/memberarea/contacts" className="contact"> <button id="btn2" onClick={() => clickHandler(2)}>                 <PermContactCalendarIcon/> Contact                  </button></Link>
+                <Link to="/memberarea/password" className="password"><button id="btn3" onClick={() => clickHandler(3)}>                 <LockIcon/> Password              </button></Link>
+                <Link to="/memberarea/address" className="address"> <button id="btn4" onClick={() => clickHandler(4)}>                  <LocationOnIcon/>Address                 </button></Link>
+                <Link to="/memberarea/bookmarks" className="bookmarks"> <button id="btn5" onClick={() => clickHandler(5)}>              <FavoriteIcon/> Bookmarks          </button></Link>
+                <Link to="/memberarea/orders" className="orders">   <button id="btn6" onClick={() => clickHandler(6)}>                  <DescriptionIcon/> Orders                    </button></Link>
+                <Link to="/memberarea/reminders" className="reminders"  ><button id="btn7" onClick={() => clickHandler(7)}>            <NotificationsIcon/> Reminders                             </button></Link>
+                <Link to="/memberarea/bonuses" className="bonuses">     <button id="btn8" onClick={() => clickHandler(8)}>              <MonetizationOnIcon/>  Bonuses                    </button></Link>
+                <Link to="/memberarea/deposite" className="deposites"> <button id="btn9" onClick={() => clickHandler(9)}>               <AccountBalanceWalletIcon/>Deposites                </button></Link>
+                <Link to="/memberarea/free-shipping" className="freeShipping"> <button id="btn10" onClick={() => clickHandler(10)}>       <LocalShippingIcon/> Free shipping                    </button> </Link>
+                <Link to="/memberarea/reviews" className="reviews">       <button id="btn11" onClick={() => clickHandler(11)}>            <ChatBubbleIcon/>  Reviews                    </button> </Link>
+                <Link to="/memberarea/recommendations" className="recommendatios"> <button id="btn12" onClick={() => clickHandler(12)}>   <ThumbUpAltIcon/> Recommendations                  </button>  </Link>
+                <hr/>
+                <a href="/" >       <button>            <ExitToAppIcon/> Log out                   </button></a>
             </aside>
             
             <main className="main">
