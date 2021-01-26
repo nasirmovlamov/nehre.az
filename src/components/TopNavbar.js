669144b5eb3dab36a5af2e4da2360defa4bacd07
Nasir <movlamovnasir2000@gmail.com>
2021-01-26 18:00:32 +0400

    new new

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,34 +8,41 @@ import member from "../assets/images/member.png"
 import sebet from "../assets/images/Sebet.png"
 import searchIcon from "../assets/images/searchIcon.png"
 import {Link} from "react-router-dom"
+import PhoneIcon from '@material-ui/icons/Phone';
 function TopNavbar() {
     return (
         <div className="topNavbar">
             <Link to="/"><img src={logoNehre} alt="" width="200" height="auto"/></Link>
-            <div>
-                <div className="searchAndSelection">
-                    <div className="selection">
-                     {/*  */}
-                        <Link to="/">
-                        </Link>
-                        {/*  */}
-                        <Link to="/">
-                        </Link>
-                    {/*  */}
-                        <Link to="/memberarea">
-                        </Link>
-                        {/*  */}
-                        <Link to="/">     
-                        </Link>
-                    </div>
+                
+                
+            <div className="searchAndLogo">    
+            <div className="searchAndPhone">
+                    <p className="phone"> <PhoneIcon/> <a href="tel:012 00 0000">012 00 0000</a></p>
                     <div className="inputAndIcon">
                         <button className="searchIcon"> <img src={searchIcon} alt="" width="20" height="auto" /></button>
                         <input type="text" placeholder="Axtarış"/>
                     </div>
                    
+            </div>
+            
+            <div className="imgAndLinks">
+                <img src={element} alt="" width="200" height="auto"/>
+                <div className="selection">
+                    {/*  */}
+                    <Link to="/">
+                    </Link>
+                    {/*  */}
+                    <Link to="/">
+                    </Link>
+                    {/*  */}
+                    <Link to="/memberarea">
+                    </Link>
+                    {/*  */}
+                    <Link to="/">     
+                    </Link>
                 </div>
             </div>
-            <img src={element} alt="" width="200" height="auto"/>
+            </div>
         </div>
     )
 }
